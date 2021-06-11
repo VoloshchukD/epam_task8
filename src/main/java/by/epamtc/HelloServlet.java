5a04c5f8291c6c3a5db6938399cbe57f03f59724
VoloshchukD <voloshchukd7@gmail.com>
2021-06-11 23:54:12 +0300

    Refactoring candies.xsd

--- src/main/java/by/epamtc/HelloServlet.java
+++ src/main/java/by/epamtc/HelloServlet.java
@@ -1,10 +1,11 @@
 package by.epamtc;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "hello", value = "/hello")
 public class HelloServlet extends HttpServlet {
     private String message;
 
@@ -12,16 +13,11 @@ public class HelloServlet extends HttpServlet {
         message = "Hello World!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("jsp/main.jsp").forward(request, response);
     }
 
     public void destroy() {
     }
+
 }
